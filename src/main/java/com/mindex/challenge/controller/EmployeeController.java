c66d7192d732d7d779c015fa6785f409d78870fe
greg.lepsch <greg.lepsch@thomsonreuters.com>
2019-03-07 17:04:19 -0500

    Task 2 commit

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.mindex.challenge.data.ReportingStructure;
 
 @RestController
 public class EmployeeController {
@@ -24,7 +23,7 @@ public class EmployeeController {
 
     @GetMapping("/employee/{id}")
     public Employee read(@PathVariable String id) {
-        LOG.debug("Received employee create request for id [{}]", id);
+        LOG.debug("Received employee read request for id [{}]", id);
 
         return employeeService.read(id);
     }
